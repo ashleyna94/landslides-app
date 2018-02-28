54107d524ce0e8639ba6ea8ccd6766827d6c80bf
ashleyna94 <31015508+ashleyna94@users.noreply.github.com>
2018-02-27 19:46:10 -0600

    added api routes

--- landslides/app.py
+++ landslides/app.py
@@ -1,8 +1,9 @@
 # import necessary libraries
-import numpy as np
-
 import os
-
+import numpy as np
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
 from flask import (
     Flask,
     render_template,
@@ -10,10 +11,6 @@ from flask import (
     request,
     redirect)
 
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-import os
 
 #################################################
 # Flask Setup
@@ -27,15 +24,15 @@ Base = automap_base()
 
 engine = create_engine(os.environ.get("DATABASE_URL"))
 
-# reflect the tables
+# Reflect the tables
 Base.prepare(engine, reflect=True)
 
-# mapped classes are now created with names by default
+# Mapped classes are now created with names by default
 # matching that of the table name.
 Landslides = Base.classes.landslides
 session = Session(engine)
 
-# create route that renders index.html template
+# Create a route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -55,6 +52,21 @@ def geo():
     }]
 
     return jsonify(geo_data)
+
+@app.route("/api/leaflet")
+def landslide_map():
+    results = session.query(Landslides.latitude, Landslides.longtiude, Landslides.landslide_size, Landslides.landslide_type, Landslides.trigger).all()
+
+    landslide_map_dict = {}
+
+    for result in results:
+        landslide_map_dict["Latitude"] = result[0]
+        landslide_map_dict["Longitude"] = result[1]
+        landslide_map_dict["Landslide Size"] = result[2]
+        landslide_map_dict["Landslide Type"]= result[3]
+        landslide_map_dict["Trigger"] = result[4]
+
+    return jsonify(landslide_map_dict)
 
 if __name__ == "__main__":
     app.run()
