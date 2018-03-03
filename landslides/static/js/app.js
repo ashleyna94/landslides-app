86a973c189a292d4e63ea74b1ce2d50dbfe35b00
akufoy <akufoy@gmail.com>
2018-03-02 22:15:01 -0600

    Update app.js

--- landslides/static/js/app.js
+++ landslides/static/js/app.js
@@ -380,47 +380,6 @@ function update(arrayNew) {
 
     var data = arrayNew;
 
-
-    var formatCount = d3.format(",.0f");
-
-    var x = d3.scaleLinear()
-        .rangeRound([0, width]);
-
-    var bins = d3.histogram()
-        .domain(x.domain())
-        .thresholds(x.ticks(20))
-        (data);
-
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(bins, function (d) { return d.length; })])
-        .range([height, 0]);
-
-
-    var bar = g.selectAll(".bar")
-        .data(bins)
-        .enter()
-        .append("g")
-        .attr("class", "bar")
-        .attr("transform", function (d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
-
-    bar.append("rect")
-        .transition()
-        .duration(1000)
-        .attr("x", 1)
-        .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
-        .attr("height", function (d) { return height - y(d.length); });
-
-    bar.append("text")
-        .attr("dy", ".75em")
-        .attr("y", 6)
-        .attr("x", (x(bins[0].x1) - x(bins[0].x0)) / 2)
-        .attr("text-anchor", "middle")
-        .text(function (d) { return formatCount(d.length); });
-
-    g.append("g")
-        .attr("class", "axis axis--x")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x));
-
 };
+  
 
